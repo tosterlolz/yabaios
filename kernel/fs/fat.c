a6191594792a7c6cccf6f68c96700bb9a0f80c2a
tosterlolz <100956266+tosterlolz@users.noreply.github.com>
2025-11-03 17:31:34 +0100

    idk

--- kernel/fs/fat.c
+++ kernel/fs/fat.c
@@ -70,14 +70,43 @@ void fat_list_files(void) {
     log_message("FAT: Root directory:");
 
     for (int i = 0; i < bpb->rootEntryCount; i++) {
+        // 0x00 = no more entries, 0xE5 = deleted
         if (entries[i].name[0] == 0x00)
             break;
-        if ((entries[i].attr & 0x0F) == 0x0F)
+        if ((entries[i].name[0] & 0xFF) == 0xE5)
+            continue;
+        // skip long-file-name entries and volume labels
+        if (entries[i].attr == 0x0F)
+            continue;
+        if (entries[i].attr & 0x08)
             continue;
 
-        char name[12];
-        memcpy(name, entries[i].name, 11);
-        name[11] = '\0';
+        // Build a printable filename: NAME.EXT (trim spaces)
+        char name[13];
+        int pos = 0;
+        // name part (8 bytes)
+        for (int j = 0; j < 8; j++) {
+            char c = entries[i].name[j];
+            if (c == ' ') break;
+            name[pos++] = c;
+        }
+        // extension part (3 bytes)
+        int extStart = pos;
+        for (int j = 8; j < 11; j++) {
+            char c = entries[i].name[j];
+            if (c == ' ') continue;
+            // if we haven't added any name chars, still append
+            if (pos == extStart) {
+                // add dot separator
+                name[pos++] = '.';
+            }
+            name[pos++] = entries[i].name[j];
+        }
+        if (pos == 0) {
+            // empty name, skip
+            continue;
+        }
+        name[pos] = '\0';
 
         log_print("  ");
         log_print(name);
